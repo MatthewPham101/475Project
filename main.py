707655082a02e746cf6b3ae7c3bd05d31223e729
Matthew Pham <79127545+MatthewPham101@users.noreply.github.com>
2024-10-27 17:09:46 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+import pandas as pd
+import json
+
+us_states = set(["AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY",
+                 "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC",
+                 "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"])
+
+
+data = []
+with open('yelp_academic_dataset_business.json', 'r') as f:  # Replace with your file path
+    for line in f:
+        record = json.loads(line)
+        if record.get("state") in us_states:
+            data.append(record)
+
+
+df_us_restaurants = pd.DataFrame(data)
+
+
+print(df_us_restaurants)
 
 
 
